8fb21fca851ab81fe2aefde936b242d04179dffd
Andrey <andriuhorak@mail.ru>
2015-08-25 23:44:15 +0300

    first_update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,12 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :set_comment, only: [:show, :destroy]
 
   # GET /comments
-  # GET /comments.json
   def index
-    @comments = Comment.all
+    @comments = Comment.order(sort_column + " " + sort_direction)
   end
 
   # GET /comments/1
-  # GET /comments/1.json
   def show
   end
 
@@ -18,19 +16,17 @@ class CommentsController < ApplicationController
   end
 
   # POST /comments
-  # POST /comments.json
   def create
     @project = Project.find(params[:project_id])
     @comment = @project.comments.build(comment_params) # Comment.new(project_id: 2)
     if @comment.save
       redirect_to @project, notice: 'Comment was successfully created.'
     else
-      render :new
+      redirect_to @project, notice: '!!! ERROR  !!! ----->Not created fill the form <--------'
     end
   end
 
   # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     @comment.destroy
 
@@ -48,4 +44,6 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:text)
     end
+
+
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,15 +2,12 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   # GET /projects
-  # GET /projects.json
   def index
     @projects = Project.all
   end
 
   # GET /projects/1
-  # GET /projects/1.json
   def show
-    @project = Project.find(params[:id])
     @comments = @project.comments
     @comment = Comment.new
   end
@@ -25,37 +22,30 @@ class ProjectsController < ApplicationController
   end
 
   # POST /projects
-  # POST /projects.json
   def create
     @project = Project.new(project_params)
 
     respond_to do |format|
       if @project.save
         format.html { redirect_to projects_url, notice: 'Project was successfully created.' }
-        format.json { render :show, status: :created, location: @project }
       else
         format.html { render :new }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /projects/1
-  # PATCH/PUT /projects/1.json
   def update
     respond_to do |format|
       if @project.update(project_params)
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { render :show, status: :ok, location: @project }
+        format.html { redirect_to projects_url, notice: 'Project was successfully updated.' }
       else
         format.html { render :edit }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /projects/1
-  # DELETE /projects/1.json
   def destroy
     @project.destroy
     respond_to do |format|
