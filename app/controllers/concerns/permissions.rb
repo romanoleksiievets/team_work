169cc89295f814b66fa65f1d69f7cb464993eb69
Lyubomyr <fedorniakl@gmail.com>
2016-03-23 10:49:06 +0200

    Refactor constrains

--- app/controllers/concerns/permissions.rb
+++ app/controllers/concerns/permissions.rb
@@ -5,26 +5,31 @@ module Permissions
   end
 
   def admin_only
-    permission_error 'Only admin can access this page.' unless current_user.admin_permissions?
+    message = 'Only admin can access this page.'
+    permission_error message unless current_user.admin_permissions?
   end
 
   def organization_admin_only
-    permission_error 'Only organization admin can access this page.' unless current_user
+    message = 'Only organization admin can access this page.'
+    permission_error message unless current_user
     .organization_admin_permissions?(current_organization)
   end
 
   def organization_member_only
-    permission_error 'Only organization member can access this page.' unless current_user
+    message = 'Only organization member can access this page.'
+    permission_error message unless current_user
     .organization_member_permissions?(current_organization)
   end
 
   def project_admin_only
-    permission_error 'Only project admin can access this page.' unless current_user
+    message = 'Only project admin can access this page.'
+    permission_error message unless current_user
     .project_admin_permissions?(current_project.try(:id))
   end
 
   def project_member_only
-    permission_error 'Only project member can access this page.' unless current_user
+    message = 'Only project member can access this page.'
+    permission_error message unless current_user
     .project_member_permissions?(current_project.try(:id))
   end
 
