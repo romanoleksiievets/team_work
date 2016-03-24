f123867b3fadcf4066d917854c59b0af71f0f25f
Vitaliy Senych <vsenych@dataxu.com>
2016-03-24 13:38:23 +0200

    Finish basic localization functionality.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,6 +49,10 @@ private
   end
 
   def set_locale
-    I18n.locale = params[:locale]
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
   end
 end
