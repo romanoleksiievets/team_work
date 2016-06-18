569d9c372fd48f5c9d8a3431f276d60973d282e8
Lyubomyr <fedorniakl@gmail.com>
2016-06-18 12:35:18 +0300

    rollback sprockets multithreads

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,6 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
-  config.assets.logger = Logger.new $stdout
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
